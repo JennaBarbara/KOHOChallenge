61f3986246fb8a1e4d384869673d49fadf887d4d
JennaBarbara <JennaBarbara@gmail.com>
2020-11-22 17:17:50 -0500

    moved velocity checks to the app file with the rest of the business logic

--- app/app.go
+++ app/app.go
@@ -3,15 +3,24 @@ package app
 import (
   "fmt"
   "log"
+  "errors"
 
   "../config"
   "../model"
   "github.com/jinzhu/gorm"
 )
 
+
 type App struct {
 	DB     *gorm.DB
 }
+
+//common errors
+var (
+  ErrExceedsDailyAmountLimit = errors.New("Requested load_amount exceeds daily limit for customer")
+  ErrExceedsWeeklyAmountLimit = errors.New("Requested load_amount exceeds weekly limit for customer")
+  ErrExceedsDailyLoadLimit = errors.New("Request exceeds daily load limit for customer")
+)
 
 // Initialize initializes the app with predefined configuration
 func (a *App) Initialize(config *config.Config) {
@@ -42,7 +51,7 @@ func LoadFunds(db *gorm.DB, req *model.LoadReq) *model.LoadResp  {
     return resp
   }
 
-  if err := model.VelocityLimitsCheck(db, loadFund); err != nil {
+  if err := velocityLimitsCheck(db, loadFund); err != nil {
     writeBadRequestResponse(resp, err)
     return resp
   }
@@ -59,4 +68,27 @@ func LoadFunds(db *gorm.DB, req *model.LoadReq) *model.LoadResp  {
 func writeBadRequestResponse(resp *model.LoadResp, err error) {
   resp.Accepted = false
   log.Printf("Request ID: %s - Error: %s ", resp.Id, err.Error())
+}
+
+//perform velocity checks described in the requirements
+func velocityLimitsCheck(db *gorm.DB,  loadFund *model.LoadedFunds) error {
+  countToday := model.CountFundsLoadedToday(db, loadFund)
+  if countToday >= 3 {
+    err := ErrExceedsDailyLoadLimit
+    return err
+  }
+
+  sumToday := model.SumFundsLoadedToday(db, loadFund) + loadFund.Load_amount
+  if sumToday > 5000.0 {
+    err := ErrExceedsDailyAmountLimit
+    return err
+  }
+
+  sumThisWeek := model.SumFundsLoadedThisWeek(db, loadFund) + loadFund.Load_amount
+  if sumThisWeek > 20000.0 {
+    err := ErrExceedsWeeklyAmountLimit
+    return err
+  }
+
+  return nil
 }
